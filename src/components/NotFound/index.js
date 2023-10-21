9caeaa310913899024a5ba9d3c25423f9b6f75a7
Thriphanindra <phanindra0041@gmail.com>
2023-10-21 07:10:42 +0000

    clear

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,21 @@
+import {
+  NotFoundContainer,
+  Images,
+  NotFoundHeading,
+  NotFoundPara,
+} from './styledComponents'
+
+const NotFound = () => (
+  <NotFoundContainer>
+    <Images
+      src="https://assets.ccbp.in/frontend/react-js/meetup/not-found-img.png"
+      alt="not found"
+    />
+    <NotFoundHeading>Page not Found</NotFoundHeading>
+    <NotFoundPara>
+      We are sorry, the page you requested could not be found.
+    </NotFoundPara>
+  </NotFoundContainer>
+)
+
+export default NotFound
